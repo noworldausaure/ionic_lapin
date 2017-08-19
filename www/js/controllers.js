1bb23a2dd247b0fed87215540e3364ea22c1d2c4
Vinarnt <contact@vinarnt.fr>
2017-08-19 16:28:52 +0200

    Fixed strips swiper

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -63,40 +63,95 @@ angular.module('starter.controllers', [])
 })
 
 .controller('StripCtrl', function($scope,$ionicPopup,$location, $rootScope, $http, Home, $stateParams, $ionicSlideBoxDelegate) {
+
   var idStories = $rootScope.idStories;
   var domain = $stateParams.domain;
   var id = $stateParams.id;
   var count = 0;
   var pubDomain = false;
   $scope.domain = domain;
-  $scope.data = [];
+
+  // Event listeners on slide modifications
+  var slideModifListeners = [];
+  function SlideModificationEvent (callback) {
+
+    this.onModification = function () {
+      callback();
+    };
+  };
 
   // SET UP SLIDER
-  $scope.data.sliderDelegate = null;
-  $scope.data.sliderOptions = {
-    initialSlide: id,
-    direction: 'horizontal', //or vertical
+  $scope.sliderOptions = {
+    zoom: true,
+    pagination: false,
+    direction: 'horizontal'
   };
 
   $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
     $scope.slider = data.slider;
+    console.log("slider initialized");
   });
 
-  $scope.$watch('data.sliderDelegate', function(newVal, oldVal) {
-    if (newVal != null) {
-      $scope.data.sliderDelegate.on('slideChangeEnd', function() {
-        $scope.data.currentPage = $scope.data.sliderDelegate.activeIndex;
-        $scope.$apply();
+  let firstStripReached = false;
+  let lastStripReached = false;
+  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data) {
+    console.log(`slide changed, index=${data.slider.activeIndex}, indexOfLast=${data.slider.slides.length - 1}, end=${data.slider.isEnd}`);
+    let index = data.slider.activeIndex;
+    let slider = $scope.slider;
+
+    // If we reach the start of the slider, we'll load data
+    if(slider.isBeginning) {
+
+      if(firstStripReached)
+        return;
+
+      let currentStripId = $scope.strips[index].id;
+      let count = Math.min(Math.max(currentStripId - 1, 0), 5);
+      let offset = Math.max(0, currentStripId - 5);
+
+      console.log(`Load ${count} strips from offset ${offset}`);
+
+      Home.returnNthStrips(
+        $http,domain, count, offset)
+      .then(function(strips) {
+
+        // If strips coming from api are already in the loaded strips
+        // that's mean we reached the first strip
+        let newStrip = strips.data[0];
+        for(let strip of $scope.strips) {
+          if (strip.id == newStrip.id) {
+            firstStripReached = true;
+            console.log("firstStrip reached");
+            return;
+          }
+        }
+
+        // Listen for slide modification to be effective
+        slideModifListeners.push(new SlideModificationEvent(function () {
+          slider.slideTo(strips.data.length, 0);
+        }));
+
+        strips.data.forEach(function(strip) {
+          $scope.strips.unshift(strip);
+          stripImageLoader(strip);
+        });
       });
     }
-  });
-
-  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data) {
-    index = data.slider.activeIndex;
     // if we reached the end of the slider, we'll load data
-    if(index == ($scope.strips.length-1)) {
+    else if(slider.isEnd) {
+
+      if(lastStripReached)
+        return;
+
       Home.returnNthStrips($http,domain,5,$scope.strips[index].id)
       .then(function(strips) {
+        if(strips.data.length == 0){
+          lastStripReached = true;
+          console.log("last strip reached");
+          return;
+        }
+
+        console.log(strips.data);
         $scope.strips = $scope.strips.concat(strips.data);
         $scope.strips.forEach(function(strip) {
           stripImageLoader(strip);
@@ -105,8 +160,41 @@ angular.module('starter.controllers', [])
     }
   });
 
+  // Watch slides array for initial slides loading and set initial slide active index when loaded
+  let slidesWatcher = $scope.$watch("slider.slides", function () {
+    let slider = $scope.slider;
+    let slideCount;
+
+    // Slide to initial slide when slides are loaded in the slider
+    if(slider != undefined && (slideCount = slider.slides.length) > 0) {
+
+      //Get the index of the queried strip
+      let initialIndex;
+      for(let i = 0; i < slideCount; i++) {
+        let strip = $scope.strips[i];
+        if(strip.id == id) {
+          initialIndex = i;
+          break;
+        }
+      }
+      console.log("Slide to init slide");
+      slider.slideTo(initialIndex, 0);
+
+      // Unwatch
+      slidesWatcher();
+    }
+  });
+
+  // Event listener for slide modifications
+  $scope.$watch("slider.slides", function () {
+    slideModifListeners.forEach(function (listener) {
+      listener.onModification();
+    });
+  });
+
   // STRIPS SECTION
-  Home.returnNthStrips($http,domain,5,id-1)
+  // Populate initial strips
+  Home.returnNthStrips($http,domain,5,id-3)
     .then(function(strips) {
       $scope.strips = strips.data;
       $scope.strips.forEach(function(strip) {
@@ -126,7 +214,6 @@ angular.module('starter.controllers', [])
   // ADVERTISING SECTION
   Domainpub = Home.returnPubByDomain($http,domain);
   Lapinpub = Home.returnLapinPub($http);
-  $scope.data = {}
 
   showConfirm = function(data) {
     var confirmPopup = $ionicPopup.confirm({
